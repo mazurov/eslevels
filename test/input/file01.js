f20c5e3ae155f893b20dd80bb17768a7ee1bdc04
Alexander MAZUROV <alexander.mazurov@gmail.com>
2013-05-11 21:49:51 +0200

    Update tests

--- test/input/file01.js
+++ test/input/file01.js
@@ -1,7 +1,13 @@
 var glob = 10;
+gl = 12;
 function Level1(param1) {
     'use strict';
     var fromLevel1 = 1;
+    try {
+
+    } catch(e) {
+        var try1, try2=e;
+    }
     return function Level2(param2) {
        var fromLevel2 = param1 + param1 + glob;
        return fromLevel2;
